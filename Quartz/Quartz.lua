58f36a3b568bfecae7a7af6382b1636c341730f8
Rainon <90132571+Nevaknight@users.noreply.github.com>
2023-01-19 07:14:42 +0300

    Add files via upload

---
+++ Quartz/Quartz.lua
@@ -0,0 +1,155 @@
+
+Quartz3DB = {
+	["namespaces"] = {
+		["Swing"] = {
+		},
+		["Buff"] = {
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["Flight"] = {
+		},
+		["Pet"] = {
+			["profiles"] = {
+				["Default"] = {
+					["x"] = 753.3333740234375,
+				},
+				["Rainon NoElvUI"] = {
+					["h"] = 32,
+					["w"] = 206,
+					["y"] = 190,
+					["strata"] = "HIGH",
+					["point"] = "BOTTOM",
+					["border"] = "Square Clean",
+					["texture"] = "Details Flat",
+					["x"] = -270,
+				},
+			},
+		},
+		["Range"] = {
+		},
+		["Mirror"] = {
+		},
+		["Player"] = {
+			["profiles"] = {
+				["Default"] = {
+					["y"] = 95.55526733398438,
+					["x"] = 1.444854736328125,
+					["point"] = "BOTTOM",
+					["hideblizz"] = false,
+				},
+				["Rainon NoElvUI"] = {
+					["y"] = 95.55526733398438,
+					["hideblizz"] = false,
+					["point"] = "BOTTOM",
+					["x"] = 1.444854736328125,
+				},
+			},
+		},
+		["GCD"] = {
+		},
+		["Focus"] = {
+			["profiles"] = {
+				["Default"] = {
+					["x"] = 753.3333740234375,
+				},
+				["Rainon NoElvUI"] = {
+					["h"] = 32,
+					["w"] = 206,
+					["y"] = 190,
+					["strata"] = "HIGH",
+					["point"] = "BOTTOM",
+					["border"] = "Square Clean",
+					["texture"] = "Details Flat",
+					["x"] = 270,
+				},
+			},
+		},
+		["Target"] = {
+			["profiles"] = {
+				["Default"] = {
+					["strata"] = "HIGH",
+					["point"] = "CENTER",
+					["iconposition"] = "left",
+					["w"] = 202,
+					["hidenametext"] = true,
+					["y"] = -196,
+					["x"] = 268,
+					["font"] = "Morpheus",
+					["iconalpha"] = 1,
+					["border"] = "Square Clean",
+					["h"] = 40,
+					["noInterruptShield"] = false,
+					["texture"] = "Details Flat",
+				},
+				["Rainon NoElvUI"] = {
+					["noInterruptShield"] = false,
+					["h"] = 40,
+					["w"] = 206,
+					["y"] = -196,
+					["x"] = 270,
+					["iconalpha"] = 1,
+					["iconposition"] = "left",
+					["strata"] = "HIGH",
+					["point"] = "CENTER",
+					["border"] = "Square Clean",
+					["hidenametext"] = true,
+					["texture"] = "Details Flat",
+					["timetextx"] = 2,
+					["font"] = "Morpheus",
+				},
+			},
+		},
+		["Interrupt"] = {
+		},
+		["EnemyCasts"] = {
+		},
+		["Latency"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Селентес - Борейская тундра"] = "Default",
+		["Клокрейн - Гордунни"] = "Default",
+		["Эури - Борейская тундра"] = "Default",
+		["Ансока - Борейская тундра"] = "Default",
+		["Корхала - Борейская тундра"] = "Default",
+		["Кэреган - Борейская тундра"] = "Rainon NoElvUI",
+		["Инлуна - Борейская тундра"] = "Default",
+		["Джэсисс - Гордунни"] = "Default",
+		["Дракрэна - Гордунни"] = "Default",
+		["Рейннон - Гордунни"] = "Default",
+		["Дайконфету - Гордунни"] = "Rainon NoElvUI",
+		["Корхалла - Азурегос"] = "Default",
+		["Мононоке - Борейская тундра"] = "Default",
+	},
+	["profiles"] = {
+		["Default"] = {
+			["modules"] = {
+				["Player"] = false,
+				["GCD"] = false,
+				["Buff"] = false,
+				["Timer"] = false,
+				["Mirror"] = false,
+				["Swing"] = false,
+			},
+		},
+		["Rainon NoElvUI"] = {
+			["completecolor"] = {
+				nil, -- [1]
+				nil, -- [2]
+				nil, -- [3]
+				1, -- [4]
+			},
+			["modules"] = {
+				["Tradeskill"] = false,
+				["Buff"] = false,
+				["Mirror"] = false,
+				["Player"] = false,
+				["GCD"] = false,
+				["Timer"] = false,
+				["Swing"] = false,
+				["Latency"] = false,
+			},
+		},
+	},
+}
